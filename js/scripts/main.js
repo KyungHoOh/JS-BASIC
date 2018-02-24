3a765699771ff1a418581678c0c15c192504047c
kyungho oh <mr.dennis.oh@gmail.com>
2018-02-24 12:31:22 +0900

    Origin
    Nothing coding

---
+++ js/scripts/main.js
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('h2');
+myHeading.innerHTML = 'Hello world!';
+
+var iceCream = 'vanila';
+if (icecream === 'vanlia') {
+	alert('Yes');}
+else{alert('Nope');
+	}
